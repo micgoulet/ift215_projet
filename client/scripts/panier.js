79470253a72c3d0f25c6151ff0ebc9204ae8a64f
micgoulet <michael.goulet2@usherbrooke.ca>
2022-12-02 13:53:42 -0500

    ajustement

--- client/scripts/panier.js
+++ client/scripts/panier.js
@@ -1,3 +1,56 @@
+function chargerpanier() {
+    $.ajax({
+        url: "/clients/" + ID_CLIENT + "/panier",
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', "Basic " + TOKEN_CLIENT);
+        },
+        error: function (result) {
+            $('#cart_details').append('<h1><a href="#/connexion">Veuillez vous connecter pour accéder au panier</a></h1>');
+        },
+        success: function (result) {
+            console.log(result)
+            $('#cart_details').html("");
+            $("#total").text(result.valeur);
+            $.each(result.items, function (key, value) {
+
+                item = $('<div></div>')
+                    .addClass('card card-body mt-4')
+                    .append('    <div class="row">\n' +
+                        '        <div class="col-lg-2 mb-4">\n' +
+                        '            <img src="images/'+ value.nomProduit +'.jpg" width="150" height="150" alt="'+ value.nomProduit +'">\n' +
+                        '        </div>\n' +
+                        '        <div class="col-lg-7">\n' +
+                        '            <h1>'+ value.nomProduit + '</h1>\n' +
+                        '            <p> Prix : \n' +
+                        '                '+ value.prix +'\n' +
+                        '            </p>\n' +
+                        '            <div class="mt-2">\n' +
+                        '                <form>\n' +
+                        '                    <label for="panier-qte'+ value.id +'" id="paniel-label-qte">Quantité :</label>\n' +
+                        '                    <input id="panier-qte'+ value.id +'" value="'+ value.quantite +'"/>\n' +
+                        '                </form>\n' +
+                        '            </div>\n' +
+                        '            <div class="mt-2">\n' +
+                        '                <button class="btn btn-primary btn-sm" type="button" onclick="modifierQte(' + value.id + ', '+ value.quantite + ')">Modifier</button>\n' +
+                        '            </div>\n' +
+                        '        </div>\n' +
+                        '        <div class="col-md-6 col-xl-3">\n' +
+                        '            <div>\n' +
+                        '                <h4>Prix : '+ value.prix * value.quantite +'</h4>\n' +
+                        '            </div>\n' +
+
+                        '            <div class="d-flex flex-column mt-4">\n' +
+                        '                <button class="btn btn-primary btn-sm" type="button" onclick="retirerProduit(' + value.id + ')">Retirer</button>\n' +
+                        '            </div>\n' +
+                        '        </div>\n' +
+                        '    </div>')
+                $('#cart_details').append(item);
+            });
+        }
+    });
+}
+
 function chargerconfirmation() {
     $.ajax({
         url: "/clients/" + ID_CLIENT + "/panier",
